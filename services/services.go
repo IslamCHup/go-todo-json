44be9443a6a0c606e0dea9bdcacc3ba319295fea
IsmaCHup <annym.gg@mail.ru>
2025-10-29 00:34:04 +0300

    ctrl + shift + f

--- services/services.go
+++ services/services.go
@@ -16,85 +16,82 @@ import (
 5. отметить выполнение
 */
 
-
 func ReadTodo() ([]structure.Todo, error) {
 	cont, err := os.ReadFile("todo.json")
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
-	
+
 	var t []structure.Todo
 
 	errorReadJS := json.Unmarshal(cont, &t)
-	if errorReadJS != nil{
+	if errorReadJS != nil {
 		return nil, errorReadJS
 	}
-
-
 
 	return t, nil
 }
 
-func WriteTodo(content []structure.Todo)error{
+func WriteTodo(content []structure.Todo) error {
 	jsdata, errorJS := json.MarshalIndent(content, "", "  ")
 
-	if errorJS != nil{
+	if errorJS != nil {
 		return fmt.Errorf("%s", errorJS)
 	}
 
 	errorWrite := os.WriteFile("todo.json", jsdata, 0644)
 
-	if errorWrite != nil{
+	if errorWrite != nil {
 		return fmt.Errorf("%s", errorWrite)
 	}
-	return 	nil
+	return nil
 }
+
 // todo >> marshal >> os.wrtie
 // content >> unmarshal >> add todo >> marshal >> os.write
-func Add(text string)error{
+func Add(text string) error {
 
 	content, err := ReadTodo()
 
-	if err != nil{
+	if err != nil {
 		return fmt.Errorf("%s", err)
 	}
-
 
 	t := structure.Todo{Text: text, Complete: false}
 	content = append(content, t)
 
 	errorsWrite := WriteTodo(content)
 
-	if errorsWrite != nil{
+	if errorsWrite != nil {
 		return fmt.Errorf("%s", errorsWrite)
 	}
 
 	return nil
 }
 
-func Remove(title string)error{
-	cont, _:= ReadTodo()
+func Remove(title string) error {
+	cont, _ := ReadTodo()
 
 	resSlice := []structure.Todo{}
-	
-	for _, val := range cont{
-		if val.Text != title{
+
+	for _, val := range cont {
+		if val.Text != title {
 			resSlice = append(resSlice, val)
 		}
-	} 
+	}
 
 	cont = resSlice
 
 	err := WriteTodo(cont)
-	if err != nil{
-		return fmt.Errorf("%s",err)
+	if err != nil {
+		return fmt.Errorf("%s", err)
 	}
 	return nil
 }
 
-func Completed(title string){
+func Completed(title string) {
 	cont, _ := ReadTodo()
-	for i := range cont{
+	for i := range cont {
 		if cont[i].Text == title {
 			cont[i].Complete = !cont[i].Complete
 		}
@@ -103,8 +100,7 @@ func Completed(title string){
 	WriteTodo(cont)
 }
 
-
-func PrintAll()error{
+func PrintAll() error {
 	content, err := ReadTodo()
 
 	if err != nil {
@@ -112,17 +108,13 @@ func PrintAll()error{
 	}
 
 	complete := ""
-	for _, v := range content{
-		if v.Complete{
+	for _, v := range content {
+		if v.Complete {
 			complete = "[v]"
-		} else{
+		} else {
 			complete = "[ ]"
 		}
 		fmt.Printf("%s %s \n", v.Text, complete)
 	}
 	return nil
 }
-
-
-
-
